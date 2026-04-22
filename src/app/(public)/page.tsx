a7b0bd8a2f814a4cf762d1f147d9b779edab424b
Leonard Deutgen <leonard@deutgen.com>
2026-04-22 13:00:00 +0200

    :art: Setup collor pattern & typography

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <main>Content</main>;
+  return (
+    <main className="bg-secondary-500 flex items-center justify-center flex-col gap-5 w-full h-dvh">
+      <h1>Detta är en h1</h1>
+      <h2>h2</h2>
+      <button className="btn btn-lg btn-primary">Kolla en knapp</button>
+    </main>
+  );
 }

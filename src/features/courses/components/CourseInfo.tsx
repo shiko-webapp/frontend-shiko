74dc791fdbcf806e3abec290e60a716c66676748
Leonard Deutgen <leonard@deutgen.com>
2026-05-03 13:44:35 +0200

    Added course single page

---
+++ src/features/courses/components/CourseInfo.tsx
@@ -0,0 +1,20 @@
+interface ICourseInfoProps {
+  lessons: number;
+  duration: string;
+  raiting: string;
+}
+export const CourseInfo = ({
+  lessons,
+  duration,
+  raiting,
+}: ICourseInfoProps) => {
+  return (
+    <div className="flex flex-wrap gap-6 text-sm text-gray-500 mb-8">
+      <span className="flex items-center gap-2">📚 {lessons} Lessons</span>
+      <span className="flex items-center gap-2">⏱️ {duration}</span>
+      <span className="flex items-center gap-2 text-orange-500">
+        ⭐ {raiting} (264 reviews)
+      </span>
+    </div>
+  );
+};

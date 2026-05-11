19d24d0496498a3706ee1e6c96e82d17d36e9566
Mikko Tirronen <mikkoatirronen@gmail.com>
2026-05-11 22:19:52 +0200

    added context for saving accesstokens

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+type AuthContextType = {
+  accessToken: string | null;
+  setAccessToken: (token: string | null) => void;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+
+  return (
+    <AuthContext.Provider value={{ accessToken, setAccessToken }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error("useAuth must be used within AuthProvider");
+  }
+
+  return context;
+}

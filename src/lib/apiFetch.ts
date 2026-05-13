4716982d21c1dcb9ecf7319c396c8969f441369a
Mikko Tirronen <mikkoatirronen@gmail.com>
2026-05-13 07:42:52 +0200

    added a custom fetch that calls refresh if it fails or routes to login

---
+++ src/lib/apiFetch.ts
@@ -0,0 +1,32 @@
+export async function apiFetch(
+  input: RequestInfo,
+  init?: RequestInit
+) {
+  let res = await fetch(input, {
+    ...init,
+    credentials: "include",
+  });
+
+  // access token expired/missing
+  if (res.status === 401) {
+    console.log("Attempting refresh...");
+
+    const refreshRes = await fetch("/api/auth/refresh", {
+      method: "POST",
+      credentials: "include",
+    });
+
+    if (!refreshRes.ok) {
+      window.location.href = "/login";
+      throw new Error("Refresh failed");
+    }
+
+    // retry original request
+    res = await fetch(input, {
+      ...init,
+      credentials: "include",
+    });
+  }
+
+  return res;
+}

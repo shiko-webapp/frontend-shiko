5d7b5d79661983aeb0542c756b2d5ef42f58104d
Mikko Tirronen <mikkoatirronen@gmail.com>
2026-05-25 23:14:22 +0200

    updated route

--- src/app/api/auth/send-verification-code/route.ts
+++ src/app/api/auth/send-verification-code/route.ts
@@ -5,7 +5,7 @@ export async function POST(req: Request) {
   const body = await req.json();
 
   const res = await apiFetch(
-    `${process.env.VERIFICATIONSERVICE_URL}/auth/begin-login`,
+    `${process.env.VERIFICATIONSERVICE_URL}/api/verification/verify`,
     {
       method: "POST",
       headers: {
@@ -17,7 +17,7 @@ export async function POST(req: Request) {
 
   if (!res.ok) {
     return NextResponse.json(
-      { message: "Begin Login failed" },
+      { message: "Verification Failed" },
       { status: 401 },
     );
   }
